f745bb37e174e6ab1e20a83af1264c56b2fc2959
Matheus Pereira <matheus.op20@aluno.ifsc.edu.br>
2021-07-18 01:01:04 -0300

    first commit

---
+++ Client/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Model
+{
+    public string date { get; set; }
+    public string time { get; set; }
+    public string username { get; set; }
+    public string message { get; set; }
+    public Model(string username, string message)
+    {
+        date = DateTime.Today.ToString("dd-MM-yyyy");
+        time = DateTime.Now.ToString("HH:mm:ss");
+        this.username = username;
+        this.message = message;
+    }
+}
+
